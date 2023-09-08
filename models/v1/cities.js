2e36adb6c104a4a14082809c6f7d587aaaa8cd98
mika <2865911620@qq.com>
2023-09-08 16:45:34 +0800

    添加城市数据库中的一些内置方法

--- models/v1/cities.js
+++ models/v1/cities.js
@@ -9,22 +9,20 @@ const citySchema = new mongoose.Schema({
 citySchema.statics.cityGuess = function (name) {
     return new Promise(async (resolve, reject) => {
         const fistWord = name.substring(0, 1).toUpperCase();
-        console.log(fistWord);
         try {
             const city = await this.findOne();
-            Object.entries(city.data).forEach(item => {
-                // if (item[0] == fistWord) {
-                //     item[1].forEach(cityItem => {
-
-                //     })
-                // }
-            })
+            const result = Object.entries(city.data).find(item => item[0] === fistWord);
+            if (result) {
+                const cityItem = result[1].find(item => item.pinyin === name);
+                resolve(cityItem);
+            }
         } catch (err) {
             reject({
                 name: 'ERROR_DATA',
-                message: '查找数据失败',
-            });
+                message: '查找城市失败'
+            })
         }
+
     })
 }
 
@@ -45,11 +43,50 @@ citySchema.statics.cityGroup = function () {
     })
 }
 
+citySchema.statics.cityHot = function () {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const city = await this.findOne();
+            resolve(city.data.hotCities);
+        } catch (err) {
+            reject({
+                name: 'ERROR_DATA',
+                message: '查找热门城市错误'
+            })
+        }
+    })
+}
+
+citySchema.statics.getCityById = function (id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const city = await this.findOne();
+            const result = Object.entries(city.data).find(item => item[0] !== 'hotCities');
+            if (result) {
+                const cityItem = result[1].find(item => Number(item.id) === id);
+                if (cityItem) {
+                    resolve(cityItem);
+                } else {
+                    resolve(null);
+                }
+            }
+
+        } catch (err) {
+            reject({
+                name: 'ERROR_DATA',
+                message: '查找城市失败',
+            })
+        }
+    })
+}
+
 const Cities = mongoose.model('Cities', citySchema);
 
-Cities.findOne((err, data) => {
+Cities.findOne().then((err, data) => {
     if (!data) {
         Cities.create({ data: cityData });
+    } else {
+        console.log(err);
     }
 })
 
