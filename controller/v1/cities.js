65d6f40e5ea85d01b6acaf81b02de2cfaadc9279
mika <2865911620@qq.com>
2023-09-08 16:44:11 +0800

    添加查找城市API

--- controller/v1/cities.js
+++ controller/v1/cities.js
@@ -1,11 +1,14 @@
 const Cities = require('../../models/v1/cities');
-const pinyin = require('pinyin');
+// const pinyin = require('pinyin');
+import pinyin from "pinyin";
+import { STYLE_NORMAL } from "pinyin";
 const AddressComponent = require('../../prototype/addressComponent');
 
 class CityHandle extends AddressComponent {
     constructor() {
         super();
         this.getCity = this.getCity.bind(this);
+        this.getExactAddress = this.getExactAddress.bind(this);
     }
 
     async getCity(req, res, next) {
@@ -15,19 +18,96 @@ class CityHandle extends AddressComponent {
             switch (type) {
                 case 'guess':
                     const city = await this.getCityName(req);
-                    cityInfo = await Cities.getGuess(city);
+                    cityInfo = await Cities.cityGuess(city);
+                    break;
+                case 'hot':
+                    cityInfo = await Cities.cityHot();
+                    break;
+                case 'group':
+                    cityInfo = await Cities.cityGroup();
+                    break;
+                default:
+                    res.json({
+                        name: 'ERROR_QUERY_TYRE',
+                        message: '参数错误',
+                    })
+                    return
             }
+            res.send(cityInfo);
         } catch (err) {
-
+            res.send({
+                name: 'ERROR_DATA',
+                message: '获取数据失败',
+            })
         }
     }
 
     async getCityName(req) {
         try {
             const cityInfo = await this.guessPostion(req);
+            /**
+             *  汉字转换成拼音
+             */
+
+            const pinyinArr = pinyin(cityInfo.city, { style: pinyin.STYLE_NORMAL });
+            let cityName = '';
+            pinyinArr.forEach(item => {
+                cityName += item[0];
+            })
+            return cityName;
+        } catch (err) {
+            return '广州';
+        }
+    }
+
+    async getCityById(req, res, next) {
+        const cityid = Number(req.params.id);
+        if (isNaN(cityid)) {
+            res.json({
+                name: 'ERROR_QUERY_TYRE',
+                message: '参数错误',
+            })
+            return
+        }
+
+        try {
+            const cityInfo = await Cities.getCityById(cityid);
+            res.send(cityInfo);
+        } catch (err) {
+            res.send({
+                name: 'ERROR_DATA',
+                message: '获取数据失败',
+            })
+        }
+    }
+
+    async getExactAddress(req, res, next) {
+        try {
+            const position = await this.geocoder(req);
+            if (position.status === 0) {
+                res.send(position.result);
+            } else {
+                throw new Error(position.message);
+            }
+        } catch (err) {
+            res.send({
+                name: "ERROR_DATA",
+                message: err,
+            })
+        }
+    }
+
+    async pois(req, res, next) {
+        try {
+            const geohash = req.params.geohash || '';
+            if (geohash.indexOf(',') === -1) {
+                
+            }
         } catch (err) {
 
         }
     }
 
 }
+
+module.exports = new CityHandle();
