72eefb49e53f8f8fe3808b62c7c7d46f60c034fe
mika <2865911620@qq.com>
2023-09-08 16:46:18 +0800

    新增城市选择路由

--- routers/v1.js
+++ routers/v1.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const Captchas = require('../controller/v1/captchas.js');
 const User = require('../controller/v2/user.js');
+const CityHandle = require('../controller/v1/cities.js');
 
 const router = express.Router();
 
@@ -13,4 +14,7 @@ router.get('/users/list', User.getUserList);
 router.get('/users/count', User.getUserCount);
 router.get('/user/city/count', User.getUserCity);
 router.get('/user/condition/userlist', User.conditionGetUser);
+router.get('/cities', CityHandle.getCity);
+router.get('/cities/:id', CityHandle.getCityById);
+router.get('/exactaddress', CityHandle.getExactAddress);
 module.exports = router;
