5896ceae414e59f5d560a85f2c3344a512b635f6
mika <2865911620@qq.com>
2023-09-14 12:01:11 +0800

    弃用formidable做表单解析

--- controller/shopping/shop.js
+++ controller/shopping/shop.js
@@ -27,176 +27,174 @@ class Shop extends AddressConpont {
             })
             return
         }
+        const form = req.body;
 
-        const form = new formidable.IncomingForm();
-        form.parse(req, async (err, fields, files) => {
-            try {
-                if (!fields.name) {
-                    throw new Error('必须填写商铺名称');
-                } else if (!fields.address) {
-                    throw new Error('必须填写商店地址');
-                } else if (!fields.phone) {
-                    throw new Error('必须填写联系电话');
-                } else if (!fields.latitude || !fields.longitude) {
-                    throw new Error('商品位置信息错误');
-                } else if (!fields.image_path) {
-                    throw new Error('必须上传图片');
-                } else if (!fields.category) {
-                    throw new Error('必须上传食品种类');
+        try {
+            if (!form.name) {
+                throw new Error('必须填写商铺名称');
+            } else if (!form.address) {
+                throw new Error('必须填写商店地址');
+            } else if (!form.phone) {
+                throw new Error('必须填写联系电话');
+            } else if (!form.latitude || !form.longitude) {
+                throw new Error('商品位置信息错误');
+            } else if (!form.image_path) {
+                throw new Error('必须上传图片');
+            } else if (!form.category) {
+                throw new Error('必须上传食品种类');
+            }
+        } catch (err) {
+            console.log('客户端参数出错', err.message);
+            res.send({
+                status: 0,
+                type: 'ERROR_PARAMS',
+                message: err.message,
+            })
+            return;
+        }
+
+        const exists = await ShopModel.findOne({ name: form.name });
+        if (exists) {
+            res.send({
+                status: 0,
+                type: 'RESTURANT_EXISTS',
+                message: '店铺已经存在，请尝试其他店铺名称',
+            })
+            return;
+        }
+
+        const opening_hours = form.startTime && form.endTime ? form.startTime + '/' + form.endTime : '8:30/20:30';
+        const newShop = {
+            name: form.name,
+            address: form.address,
+            description: form.description || '',
+            float_delivery_fee: form.float_delivery_fee || 0,
+            float_minimum_order_amount: form.float_minimum_order_amount || 0,
+            id: restaurant_id,
+            is_premium: form.is_premium || false,
+            is_new: form.new || false,
+            latitude: form.latitude,
+            longitude: form.longitude,
+            location: [form.longitude, form.latitude],
+            opening_hours: [opening_hours],
+            phone: form.phone,
+            promotion_info: form.promotion_info || '欢迎光临,用餐高峰期请提前下单,谢谢',
+            rating: (4 + Math.random()).toFixed(1),
+            rating_count: Math.ceil(Math.random() * 1000),
+            recent_order_num: Math.ceil(Math.random() * 1000),
+            status: Math.round(Math.random()),
+            image_path: form.image_path,
+            category: form.category,
+            piecewise_agent_fee: { tips: '配送费￥' + (form.float_delivery_fee || 0), },
+            activities: [],
+            supports: [],
+            license: {
+                business_license_image: form.business_license_image || '',
+                catering_service_license_image: form.catering_service_license_image || ''
+            },
+            identification: {
+                company_name: '',
+                identificate_agency: '',
+                identificate_date: '',
+                legal_person: '',
+                licenses_date: '',
+                licenses_number: '',
+                licenses_scope: '',
+                operation_period: '',
+                registered_address: '',
+                registered_number: ''
+            }
+        }
+
+        //配送方式
+        if (form.delivery_mode) {
+            Object.assign(newShop, {
+                delivery_mode: {
+                    color: '57a9ff',
+                    id: 1,
+                    is_solid: true,
+                    text: '厨鲜生快达'
                 }
-            } catch (err) {
-                console.log('客户端参数出错', err.message);
-                res.send({
-                    status: 0,
-                    type: 'ERROR_PARAMS',
-                    message: err.message
-                })
-                return
-            }
-            const exists = await ShopModel.findOne({ name: fields.name });
-            if (exists) {
-                res.send({
-                    status: 0,
-                    type: 'RESTURANT_EXISTS',
-                    message: '店铺已经存在,请尝试其他店铺名称'
-                })
-                return
-            }
-            const opening_hours = fields.startTime && fields.endTime ? fields.startTime + '/' + fields.endTime : '8:30/20:30';
-            const newShop = {
-                name: fields.name,
-                address: fields.address,
-                description: fields.description || '',
-                float_delivery_fee: fields.float_delivery_fee || 0,
-                float_minimum_order_amount: fields.float_minimum_order_amount || 0,
-                id: restaurant_id,
-                is_premium: fields.is_premium || false,
-                is_new: fields.new || false,
-                latitude: fields.latitude,
-                longitude: fields.longitude,
-                location: [fields.longitude, fields.latitude],
-                opening_hours: [opening_hours],
-                phone: fields.phone,
-                promotion_info: fields.promotion_info || '欢迎光临,用餐高峰期请提前下单,谢谢',
-                rating: (4 + Math.random()).toFixed(1),
-                rating_count: Math.ceil(Math.random() * 1000),
-                recent_order_num: Math.ceil(Math.random() * 1000),
-                status: Math.round(Math.random()),
-                image_path: fields.image_path,
-                category: fields.category,
-                piecewise_agent_fee: {
-                    tips: '配送费￥' + (fields.float_delivery_fee || 0),
-                },
-                activities: [],
-                supports: [],
-                license: {
-                    business_license_image: fields.business_license_image || '',
-                    catering_service_license_image: fields.catering_service_license_image || ''
-                },
-                identification: {
-                    company_name: '',
-                    identificate_agency: '',
-                    identificate_date: '',
-                    legal_person: '',
-                    licenses_date: '',
-                    licenses_number: '',
-                    licenses_scope: '',
-                    operation_period: '',
-                    registered_address: '',
-                    registered_number: ''
+            })
+        }
+
+        // let activities = JSON.parse(form.activities);
+
+        if (form.activities) {
+            form.activities.forEach((item, index) => {
+                switch (item.icon_name) {
+                    case '减':
+                        item.icon_color = 'f07373';
+                        item.id = index + 1;
+                        break;
+                    case '特':
+                        item.icon_color = 'edc123';
+                        item.id = index + 1;
+                        break;
+                    case '新':
+                        item.icon_color = '70bc46';
+                        item.id = index + 1;
+                        break;
+                    case '领':
+                        item.icon_color = 'e3ee0d';
+                        item.id = index + 1;
+                        break;
                 }
+                newShop.activities.push(item)
+            })
+        }
 
-            }
-            //配送方式
-            if (fields.delivery_mode) {
-                Object.assign(newShop, {
-                    delivery_mode: {
-                        color: '57a9ff',
-                        id: 1,
-                        is_solid: true,
-                        text: '厨鲜生快达'
-                    }
-                })
-            }
-            // let activities = JSON.parse(fields.activities);
-            // console.log(fields.activities);
-            //商店支持的活动
-            if (fields.activities) {
-                fields.activities.forEach((item, index) => {
-                    switch (item.icon_name) {
-                        case '减':
-                            item.icon_color = 'f07373';
-                            item.id = index + 1;
-                            break;
-                        case '特':
-                            item.icon_color = 'edc123';
-                            item.id = index + 1;
-                            break;
-                        case '新':
-                            item.icon_color = '70bc46';
-                            item.id = index + 1;
-                            break;
-                        case '领':
-                            item.icon_color = 'e3ee0d';
-                            item.id = index + 1;
-                            break;
+        if (form.bao) {
+            newShop.supports.push({
+                description: '已加入"外卖保"计划,食品安全有保障',
+                icon_color: '999999',
+                icon_name: '保',
+                id: 7,
+                name: '外卖保'
+            })
+        }
 
-                    }
-                    newShop.activities.push(item)
+        if (form.zhun) {
+            newShop.supports.push({
+                description: '准时到达,超时赔偿',
+                icon_color: '57a9ff',
+                icon_name: '准',
+                id: 9,
+                name: '准时达'
+            })
+        }
 
-                })
-            }
-            if (fields.bao) {
-                newShop.supports.push({
-                    description: '已加入"外卖保"计划,食品安全有保障',
-                    icon_color: '999999',
-                    icon_name: '保',
-                    id: 7,
-                    name: '外卖保'
-                })
-            }
+        if (form.piao) {
+            newShop.supports.push({
+                description: '该商家支持开据发票,请在下单前填写好发票抬头',
+                icon_color: '999999',
+                icon_name: '票',
+                id: 4,
+                name: '开发票',
+            })
+        }
 
-            if (fields.onTime) {
-                newShop.supports.push({
-                    description: '准时到达,超时赔偿',
-                    icon_color: '57a9ff',
-                    icon_name: '准',
-                    id: 9,
-                    name: '准时达'
-                })
-            }
+        try {
+            //保存数据，并增加对应的食品种类的数量
+            const shop = new ShopModel(newShop);
+            await shop.save();
+            CategoryHandle.addCategory(form.category);
+            Rating.initData(restaurant_id);
+            Food.initData(restaurant_id);
+            res.send({
+                status: 1,
+                message: '添加场馆成功',
+                shopDetail: newShop,
+            })
+        } catch (err) {
+            res.send({
+                status: 0,
+                type: 'ERROR_SERVER',
+                message: '添加商铺失败',
+            })
+        }
 
-            if (fields.bill) {
-                newShop.supports.push({
-                    description: '该商家支持开据发票,请在下单前填写好发票抬头',
-                    icon_color: '999999',
-                    icon_name: '票',
-                    id: 4,
-                    name: '开发票'
-                })
-            }
-
-            try {
-                //保存数据,并增加对应的食品种类的数量
-                const shop = new ShopModel(newShop);
-                await shop.save();
-                CategoryHandle.addCategory(fields.category);
-                Rating.initData(restaurant_id);
-                Food.initData(restaurant_id);
-                res.send({
-                    status: 1,
-                    type: '添加餐馆成功',
-                    shopDetail: newShop,
-                })
-
-            } catch (err) {
-                res.send({
-                    status: 0,
-                    type: 'ERROR_SERVER',
-                    message: '添加商铺失败'
-                })
-            }
-        })
+        
 
     }
 
