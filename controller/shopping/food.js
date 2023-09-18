02d92467b4e733321ce216718e6aa4ae6cc9f902
mika <2865911620@qq.com>
2023-09-18 19:46:25 +0800

    新增获取餐馆种类API

--- controller/shopping/food.js
+++ controller/shopping/food.js
@@ -26,6 +26,7 @@ class Food extends BaseComponent {
         this.initData = this.initData.bind(this);
     }
 
+    //每个餐馆都有一个初始化评论数据
     async initData(restaurant_id) {
         for (let i = 0; i < this.defaultData.length; i++) {
             let category_id;
@@ -48,6 +49,30 @@ class Food extends BaseComponent {
         }
     }
 
+    //获取特定餐馆种类
+    async getCategory(req, res, next) {
+        const restaurant_id = req.params.restaurant_id;
+        try {
+            const category_list =await MenuModel.find({ restaurant_id }, { _id: 0, __v: 0 });
+            res.send({
+                status: 1,
+                category_list,
+            })
+        } catch (err) {
+            console.log(err);
+            res.send({
+                status: 0,
+                type: 'ERROR_GET_DATA',
+                message: '获取数据失败',
+            })
+        }
+    }
+
+
+    //添加食品种类
+    async addCategory(req,res,next){
+        
+    }
 }
 
 module.exports = new Food();
