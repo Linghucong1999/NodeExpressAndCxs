7586134c3518c4612a90631caa247a2dcfed9a84
mika <2865911620@qq.com>
2024-01-06 02:19:15 +0800

    test:测试pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "FreshGoServer",
-    script: "./index.js",
+    script: "index.js",
     watch: true,   //启动热更新
     cwd:'..',
     log_date_format: "YYYY-MM-DD HH:mm Z",  //日志时间格式
@@ -10,11 +10,12 @@ module.exports = {
     max_restarts: 10,  //最大重启数
     restart_delay: 4000,  //延时重启时间
     autorestart: true,  //自动重启
-    exec_mode: "cluster",  //多进程模式
+    // exec_mode: "cluster",  //多进程模式
     ignore_watch: [                           // 不用监听的文件
       "node_modules",
       "logs"
     ],
     windowsHide: true,
+    restart_delay:1000,
   }]
 }
